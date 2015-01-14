67ce46e5c9a31a870e37ab35c6e47135867665a0
Charlie Brown <carbonrobot@gmail.com>
2015-01-14 13:49:49 -0600

    Added additional criteria for searching

--- src/CCS.Services/Criteria/PatientSearchCriteria.cs
+++ src/CCS.Services/Criteria/PatientSearchCriteria.cs
@@ -6,10 +6,15 @@
 
     public class PatientSearchCriteria : SearchCriteria<Patient>
     {
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         
         public override IQueryable<Patient> ApplyTo(IQueryable<Patient> query)
         {
+            if (!string.IsNullOrEmpty(this.FirstName))
+            {
+                query = query.Where(x => String.Equals(x.NameFirst, this.FirstName, StringComparison.InvariantCultureIgnoreCase));
+            }
             if (!string.IsNullOrEmpty(this.LastName))
             {
                 query = query.Where(x => String.Equals(x.NameLast, this.LastName, StringComparison.InvariantCultureIgnoreCase));
